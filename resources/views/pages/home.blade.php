f2ad084caf48f8eadf2aa97523baf7cdd114e49d
codingcele <marcocelegato9@gmail.com>
2023-02-18 15:33:28 +0100

    front end people

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
     <div class="container">
-        <h1>Hello</h1>
+        <h1>People</h1>
+
+        <ul>
+            @foreach ($people as $person)
+                
+                <li>
+                    {{ $person -> name }} - {{ $person -> mail }}
+                    <br>
+                    Phone: {{ $person -> personDetail -> phone }}
+                    <br>
+                    Address: {{ $person -> personDetail -> address }}
+                    <hr>
+                </li>
+
+            @endforeach
+        </ul>
+
     </div>
 @endsection
